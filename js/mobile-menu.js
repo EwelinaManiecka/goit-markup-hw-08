9ae11ae27cb971e51b7bd8ad778c724a6b1251f4
EwelinaManiecka <ewelina.maniecka@op.pl>
2022-07-12 17:41:10 +0200

    mobile-menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,7 +1,7 @@
 (() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const openMenuBtn = document.querySelector('.mobile-menu__btn');
+    const closeMenuBtn = document.querySelector('.mobile-menu__btn--close');
   
     const toggleMenu = () => {
       const isMenuOpen =
