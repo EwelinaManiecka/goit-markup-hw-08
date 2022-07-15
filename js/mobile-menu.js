c8f162f17d638f800720d82812a37fe356e5f560
EwelinaManiecka <ewelina.maniecka@op.pl>
2022-07-15 14:46:46 +0200

    portfolio mobile-menu funcciona

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -26,3 +26,19 @@
       bodyScrollLock.enableBodyScroll(document.body);
     });
   })();
+
+  
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-mobile-open]"),
+      closeModalBtn: document.querySelector("[data-mobile-close]"),
+      modal: document.querySelector("[data-mobile]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
